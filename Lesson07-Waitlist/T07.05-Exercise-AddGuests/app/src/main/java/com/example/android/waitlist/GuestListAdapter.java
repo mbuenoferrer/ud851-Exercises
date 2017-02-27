af291afa782b754fdad84b6db33d1f2049b6e6f8
mbuenoferrer <mbuenoferrer@gmail.com>
2017-02-27 21:06:44 +0100

    Insert new row into SQLite database

--- Lesson07-Waitlist/T07.05-Exercise-AddGuests/app/src/main/java/com/example/android/waitlist/GuestListAdapter.java
+++ Lesson07-Waitlist/T07.05-Exercise-AddGuests/app/src/main/java/com/example/android/waitlist/GuestListAdapter.java
@@ -19,8 +19,9 @@ public class GuestListAdapter extends RecyclerView.Adapter<GuestListAdapter.Gues
 
     /**
      * Constructor using the context and the db cursor
+     *
      * @param context the calling context/activity
-     * @param cursor the db cursor with waitlist data to display
+     * @param cursor  the db cursor with waitlist data to display
      */
     public GuestListAdapter(Context context, Cursor cursor) {
         this.mContext = context;
@@ -57,13 +58,23 @@ public class GuestListAdapter extends RecyclerView.Adapter<GuestListAdapter.Gues
         return mCursor.getCount();
     }
 
-    // TODO (15) Create a new function called swapCursor that takes the new cursor and returns void
+    // COMPLETED (15) Create a new function called swapCursor that takes the new cursor and returns void
+    public void swapCursor(Cursor cursor) {
+        // COMPLETED (16) Inside, check if the current cursor is not null, and close it if so
+        if(mCursor != null){
+            mCursor.close();
+        }
 
-    // TODO (16) Inside, check if the current cursor is not null, and close it if so
+        // COMPLETED (17) Update the local mCursor to be equal to  newCursor
+        mCursor = cursor;
 
-    // TODO (17) Update the local mCursor to be equal to  newCursor
+        // COMPLETED (18) Check if the newCursor is not null, and call this.notifyDataSetChanged() if so
+        if(mCursor != null){
+            this.notifyDataSetChanged();
+        }
 
-    // TODO (18) Check if the newCursor is not null, and call this.notifyDataSetChanged() if so
+    }
+
 
     /**
      * Inner class to hold the views needed to display a single item in the recycler-view
